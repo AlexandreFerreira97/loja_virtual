c52215d78dc580416c17e5cd067e179c71186f2f
Alexandre <alexandrefaria3012@gmail.com>
2023-08-08 17:04:25 -0300

    Update category_tile.dart

--- lib/tiles/category_tile.dart
+++ lib/tiles/category_tile.dart
@@ -1,5 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:loja_virtual/screens/category_screen.dart';
 
 class CategoryTile extends StatelessWidget {
   const CategoryTile(this.snapshot{Key? key}) : super(key: key);
@@ -8,15 +9,18 @@ class CategoryTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const ListTile(
+    return ListTile(
       leading: CircleAvatar(radius: 25.0,
         backgroundColor: Colors.transparent,
         backgroundImage: NetworkImage(snapshot.data['icon']),
       ),
       title: Text(snapshot.data['title']),
-      trailing: Icon(Icons.keyboard_arrow_right),
+      trailing: const Icon(Icons.keyboard_arrow_right),
       onTap: (){
-
+        Navigator.of(context).push(
+        MaterialPageRoute(builder: (context) => CategoryScreen(snapshot)
+          ),
+        );
       },
     );
   }
