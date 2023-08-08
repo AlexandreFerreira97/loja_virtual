8a781b6a5eede0641d40ae40a177bb4f8e2a305e
Alexandre <alexandrefaria3012@gmail.com>
2023-08-08 17:01:03 -0300

    Create category_screen.dart

---
+++ lib/screens/category_screen.dart
@@ -0,0 +1,13 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+
+class CategoryScreen extends StatelessWidget {
+  const CategoryScreen(this.documentSnapshot {Key? key}) : super(key: key);
+
+  final DocumentSnapshot documentSnapshot;
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
